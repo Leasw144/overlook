0a549b74079c8d0c073f9dc98fbcaf5775688b6e
Sean Leas <phoenix_24leas.com>
2020-07-31 16:15:30 -0600

    Initialize Bookings constructor #7

---
+++ src/Bookings.js
@@ -0,0 +1,10 @@
+class Bookings {
+  constructor(userID, date, roomNumber) {
+    this.userID = userID
+    this.date = date
+    this.roomNumber = roomNumber
+    this.roomServiceCharges
+  }
+}
+
+export default Bookings

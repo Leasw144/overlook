d8a1b5c0af6f429780b2d4c751b243e6dd74e603
Sean Leas <phoenix_24leas.com>
2020-08-01 00:27:34 -0600

    Create m to  make username log #4

--- src/User.js
+++ src/User.js
@@ -1,25 +1,32 @@
 class User {
-  constructor(username) {
-    this.username = username
+  constructor(guestsData) {
+    // this.username = username || 0
     this.password = 'overlook2020'
-    this.allUsers = []
+    this.allUsers = guestsData
     this.allRooms = []
     this.allBookings = []
+    
+    this.createUsernames(guestsData)
+  }
+  
+  createUsernames(guests) {
+    const allUsernames = guests.map(guest => `customer${guest.id}`)
+    this.usernameLog = allUsernames
   }
 
-  checkPassword(usernameAttempt, pwAttempt) {
-    if(usernameAttempt === this.username && pwAttempt === this.password) {
+  checkValidation(usernameAttempt, pwAttempt) {
+    if (this.usernameLog.includes(usernameAttempt) && pwAttempt === this.password) {
       return true
     } else {
       return `Username or Password was entered incorrectly`
     }
   }
-  findBookings(id) {
-    const personalBookings = bookings.filter(booking => {
-      return booking.userID === this.guestID
-    })
-    this.personalBookings = personalBookings
-  }
+  // findBookings(id) {
+  //   const personalBookings = bookings.filter(booking => {
+  //     return booking.userID === this.guestID
+  //   })
+  //   this.personalBookings = personalBookings
+  // }
 }
 
 export default User

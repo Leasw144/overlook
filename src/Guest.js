31b98d7db01d81cc0fa956dea8b7f607ce76dbe6
Sean Leas <phoenix_24leas.com>
2020-07-31 19:15:46 -0600

    Remove uneeded properties, c/o findBookings

--- src/Guest.js
+++ src/Guest.js
@@ -1,18 +1,19 @@
+import User from '../src/User';
+
 class Guest extends User {
   constructor(guestID, name) {
-    super()
+    super(`customer${guestID}`)
     this.guestID = guestID
     this.name = name
-    this.username = `customer${this.guestID}`
-    this.findBookings(guestID)
   }
 
-  findBookings(id) {
-    const personalBookings = bookings.filter(booking => {
-      return booking.userID === this.guestID
-    })
-    this.personalBookings = personalBookings
-  }
+  //not sure if findBookings should go here
+  // findBookings(id) { 
+  //   const personalBookings = bookings.filter(booking => {
+  //     return booking.userID === this.guestID
+  //   })
+  //   this.personalBookings = personalBookings
+  // }
 }
 
 export default Guest

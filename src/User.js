7d9b4b459ae3f692d83ea9cad826a5ac83f7089e
Sean Leas <phoenix_24leas.com>
2020-08-02 18:11:51 -0600

    Refactor User to hold more data, fix createUsername

--- src/User.js
+++ src/User.js
@@ -1,23 +1,25 @@
 class User {
-  constructor(guestsData) {
+  constructor(guestsData, roomData, bookingsData) {
     // this.username = username || 0
     this.password = 'overlook2020'
     this.allUsers = guestsData
-    this.allRooms = []
-    this.allBookings = []
-    this.createUsernames(guestsData)
+    this.allRooms = roomData
+    this.allBookings = bookingsData
+    // this.allUsernames = this.createUsernames()
   }
   
-  createUsernames(guests) {
-    const allUsernames = guests.map(guest => `customer${guest.id}`)
+  createUsernames() {
+    console.log('createUser', this.allUsers.users)
+    const allUsernames = this.allUsers.users.map(guest => `customer${guest.id}`)
+    console.log(allUsernames)
     this.usernameLog = allUsernames
   }
 
   checkValidation(usernameAttempt, pwAttempt) {
     if (this.usernameLog.includes(usernameAttempt) && pwAttempt === this.password) {
-      return true
+      return 1
     } else if (usernameAttempt === 'manager' && pwAttempt === this.password) {
-      return true
+      return 0
     } 
     return `Username or Password was entered incorrectly`
   }

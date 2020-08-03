a8c600df8795238798160f1f7327dfc6b9b80d33
Sean Leas <phoenix_24leas.com>
2020-08-02 18:11:28 -0600

    Parse fetchData functs, create eventHandler

--- src/index.js
+++ src/index.js
@@ -1,48 +1,66 @@
 // import fetchData from './fetchData';
 import moment from 'moment';
 let todaysDate = moment().format("YYYY/MM/DD");
-let userData, bookingData, roomData
 
+let hotel = null
 
-function fetchData() {
-  userData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+function fetchUserData() {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
     .then(data => data.json())
-    .then(data => data.userData)
-    .then(data => console.log(data))
     .catch(error => console.log('userData error'))
+}
 
-  bookingData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+function fetchBookingData() {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
     .then(data => data.json())
-    .then(data => data.bookingData)
     .catch(error => console.log('bookingData error'))
+}
 
-  roomData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+function fetchRoomData() {
+  return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
     .then(data => data.json())
-    .then(data => data.bookingData)
     .catch(error => console.log('bookingData error'))
+}
 
-  Promise.all([userData, bookingData, roomData])
+function fetchAllData() {
+  const userData = fetchUserData()
+  const bookingData = fetchBookingData()
+  const roomData = fetchRoomData()
+  
+  return Promise.all([userData, bookingData, roomData])
     .then(data => {
-      let allData = {}
-      // console.log(allData)
-      allData.userData = data[0]
-      allData.bookingData = data[1]
-      allData.roomData = data[2]
-      return allData
-
-      
+      hotel = new User(data[0], data[1], data[2])
+      hotel.createUsernames(data[0])
+      console.log(hotel)
+      // let allData = {}
+      // allData.userData = data[0]
+      // allData.bookingData = data[1]
+      // allData.roomData = data[2]
+      // instantiateData(allData)
+      return hotel
     })
 }
 
 
-window.addEventListener('load', fetchData)
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+const clickhandler = () => {
+  // const submitLogin = document.querySelector('.submit')
+  if (event.target.closest('.login-submit')) {
+    const userAttempt = document.querySelector('.username-input').value
+    const pwAttempt = document.querySelector('.pw-input').value
+    const loginOutcome = hotel.checkValidation(userAttempt, pwAttempt)
+    domUpdates.determineDash(loginOutcome)
+  }
+}
+
+
+window.addEventListener('load', fetchAllData)
+window.addEventListener('click', clickhandler)
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
  
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+import User from './User';
+import domUpdates from './domUpdates'
 // console.log('This is the JavaScript entry file - your code begins here.');

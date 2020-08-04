cf6c10390182d48d14ae344e5dcd9a9284469ced
Sean Leas <phoenix_24leas.com>
2020-08-04 02:20:01 -0600

    Comment out sendToDash, build on sendToGuestDash #19

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,16 +1,16 @@
 const domUpdates = {
   currentUser: null,
   todaysDate: null,
-  determineDash(outcome) {
-    if (outcome === 1) {
-      console.log('it worked')
-      this.sendToGuestDash()
-    } else if (outcome === 0) {
-      this.sendToManagerDash(this.todaysDate) 
-    } else if (outcome === `Username or Password was entered incorrectly`) {
-      this.displayLoginError(outcome)
-    }
-  },
+  // determineDash(outcome) {
+  //   if (outcome === 1) {
+  //     console.log('it worked')
+  //     this.sendToGuestDash()
+  //   } else if (outcome === 0) {
+  //     this.sendToManagerDash(this.todaysDate) 
+  //   } else if (outcome === `Username or Password was entered incorrectly`) {
+  //     this.displayLoginError(outcome)
+  //   }
+  // },
 
   sendToManagerDash(date) {
     document.querySelector('.login-box').classList.add('hidden')
@@ -21,12 +21,18 @@ const domUpdates = {
   updateManagerStats(date) {
     console.log(this.currentUser)
     document.querySelector('.total-rooms-available').innerText = this.currentUser.findTotalOpenRooms(date)
-    document.querySelector('.todays-revenue').innerText = `$${this.currentUser.calcRevenue(date)}`
+    document.querySelector('.todays-revenue').innerText = this.currentUser.calcRevenue(date)
     document.querySelector('.occupancy').innerText = `${this.currentUser.calcPercentageOccupied(date)}%`
   },
 
   sendToGuestDash() {
-    console.log('working')
+    document.querySelector('.login-box').classList.add('hidden')
+    document.querySelector('.guest-display').classList.remove('hidden')
+    this.updateGuestStats(date)
+  },
+
+  updateGuestStats(date) {
+    console.log(this.currentUser)
   },
 
   displayLoginError(outcome) {

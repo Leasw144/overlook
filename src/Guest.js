d1f0c22d6abf8508f3feb657ba622c61e8cacf35
Sean Leas <phoenix_24leas.com>
2020-08-03 22:00:58 -0600

    Create funct to find Username #19

--- src/Guest.js
+++ src/Guest.js
@@ -1,19 +1,26 @@
-import User from '../src/User';
+import Hotel from '../src/Hotel';
 
-class Guest extends User {
-  constructor(guestID, name) {
-    super(`customer${guestID}`)
+class Guest extends Hotel {
+  constructor(guestID, guestsData, roomData, bookingsData) {
+    super(guestsData, roomData, bookingsData)
     this.guestID = guestID
-    this.name = name
+    this.findName(this.guestID)
   }
 
+  findName(usernameID) {
+    const signedIn = this.allUsers.find(guest => {
+ 
+      return guest.id === Number(usernameID)
+    })
+    this.name = signedIn.name
+  }
   //not sure if findBookings should go here
-  // findBookings(id) { 
-  //   const personalBookings = bookings.filter(booking => {
-  //     return booking.userID === this.guestID
-  //   })
-  //   this.personalBookings = personalBookings
-  // }
+  findBookings(id) { 
+    const personalBookings = bookings.filter(booking => {
+      return booking.userID === this.guestID
+    })
+    this.personalBookings = personalBookings
+  }
 }
 
 export default Guest
